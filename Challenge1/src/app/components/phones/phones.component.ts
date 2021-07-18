6c1857e5455886a5923afeb977843d4b56bc072f
Jakub <jradzik4@gmail.com>
2021-07-18 20:05:15 +0200

    few changes

--- Challenge1/src/app/components/phones/phones.component.ts
+++ Challenge1/src/app/components/phones/phones.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-phones',
   templateUrl: './phones.component.html',
-  styleUrls: ['./phones.component.css']
+  styleUrls: ['./phones.component.css', './phones.component.mobile.css']
 })
 export class PhonesComponent implements OnInit {
 
