659007f7d2164e51afb17e55deb4444fd60448f4
Jakub Radzik <jradzik4@gmail.com>
2021-08-24 20:35:51 +0200

    init project challange 6

--- Challenge6/src/app/app.module.ts
+++ Challenge6/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { SearchComponent } from './header/search/search.component';
+import { TabComponent } from './header/tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent,
+    SearchComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule
