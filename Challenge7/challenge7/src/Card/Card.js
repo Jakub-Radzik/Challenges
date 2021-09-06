784573d1eaab75ab14f09535d2fe21257ebdbb15
Jakub <jradzik4@gmail.com>
2021-09-06 20:11:59 +0200

    a lot of stuff

--- Challenge7/challenge7/src/Card/Card.js
+++ Challenge7/challenge7/src/Card/Card.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../Styles/Card/Card.sass';
+import ProfileLink from './ProfileLink/ProfileLink';
 
 const Card = ({ item }) => {
-  const { login, id, avatar_url, html_url } = item;
+  const {
+    login,
+    avatar_url,
+    html_url,
+    followers_url,
+    following_url,
+    repos_url,
+    url,
+  } = item;
 
-  React.useEffect(() => {
-    console.log(item);
-  }, [item]);
+  // React.useEffect(() => {
+  //   console.log(item);
+  // }, [item]);
 
   return (
     <div className="Card">
-      <a href={html_url}>
-        <div>{login}</div>
-      </a>
-      <div>{id}</div>
-      <img src={avatar_url} alt="alter" />
+      <div className="left">
+        <img src={avatar_url} alt="alter" />
+      </div>
+      <div className="right">
+        <div className="top">
+          <a href={html_url}>
+            <h1>{login}</h1>
+          </a>
+        </div>
+        <div className="bottom">
+          <ProfileLink text={'Followers'} url={followers_url} />
+          <ProfileLink text={'Following'} url={following_url} />
+          <ProfileLink text={'Repositories'} url={repos_url} />
+          <ProfileLink text={'More Information'} url={url} />
+        </div>
+      </div>
     </div>
   );
 };
