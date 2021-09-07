f3f48c67d21affd19142fd325b478c3f8295b985
Jakub Radzik <jradzik4@gmail.com>
2021-09-07 13:26:32 +0200

    pagination

--- Challenge7/challenge7/src/App.js
+++ Challenge7/challenge7/src/App.js
@@ -6,6 +6,7 @@ import Card from './Card/Card';
 import Loader from './Utils/Loader/Loader';
 import Error from './Utils/Error/Error';
 import Header from './Components/Header/Header';
+import ReactPaginate from 'react-paginate';
 
 function App() {
   const [searchTerm, setSearchTerm] = React.useState('');
@@ -18,16 +19,19 @@ function App() {
     setSearchTerm(event.target.value);
   };
 
-  const handlerClick = () => {
+  const handlerClick = (page = 1) => {
     setLoadingResult(true);
-
-    const page = 1;
     const per_page = 10;
     axios
       .get(
-        `https://api.github.com/search/users?q=${searchTerm}&page=${1}&per_page=${per_page}`
+        `https://api.github.com/search/users?q=${searchTerm}&page=${page}&per_page=${per_page}`
       )
       .then((r) => {
+        if (r.data.total_count >= 1000) {
+          setSites(Math.ceil(1000 / per_page) - 2); //1000 is last result we can get from API
+        } else {
+          setSites(Math.ceil(r.data.total_count / per_page));
+        }
         setSearchResult(r.data.items);
         setLoadingResult(false);
       })
@@ -38,22 +42,37 @@ function App() {
       });
   };
 
+  const pageControl = (activePage) => {
+    handlerClick(activePage);
+  };
+
+  const [sites, setSites] = React.useState(0);
+
   return (
     <div className="App">
       <Header>
         <h1>devfinder</h1>
         <div>switch</div>
       </Header>
-
-      <Search
-        placeholder={'Search GitHub username...'}
-        searchTerm={searchTerm}
-        searchTermHandler={searchTermChangeHandler}
-        submitHandler={() => handlerClick()}
-        isAutoFocus={true}
-      >
-        <button onClick={() => handlerClick()}>Search</button>
-      </Search>
+      <div className="tools">
+        <Search
+          placeholder={'Search GitHub username...'}
+          searchTerm={searchTerm}
+          searchTermHandler={searchTermChangeHandler}
+          submitHandler={() => handlerClick()}
+          isAutoFocus={true}
+        >
+          <button onClick={() => handlerClick()}>Search</button>
+        </Search>
+        {sites > 0 && (
+          <ReactPaginate
+            pageCount={sites}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={1}
+            onPageChange={(page) => pageControl(page.selected + 1)}
+          />
+        )}
+      </div>
       <div>
         {loadingResult && <Loader />}
         {errorResult && <Error />}
